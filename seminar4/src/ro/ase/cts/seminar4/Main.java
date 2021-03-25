9f7eaffb845ebf1a2a067f406ea56825b0d851ef
AdrianaPoi <poinarescuadriana18@stud.ase>
2021-03-25 08:37:15 +0200

    seminar5 break

--- seminar4/src/ro/ase/cts/seminar4/Main.java
+++ seminar4/src/ro/ase/cts/seminar4/Main.java
@@ -1,9 +1,25 @@
 package ro.ase.cts.seminar4;
 
+import ro.ase.cts.seminar4.EagerShoppingCartExample;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ShoppingCart myCart=ShoppingCart.getInstance();
-	}
+		Cart shoppingCart = Cart.getInstance("shoppingCart");
+		Cart wishList = Cart.getInstance("wishlist");
 
+		shoppingCart.getProducts();
+		wishList.getProducts();
+
+		EagerShoppingCartExample example = EagerShoppingCartExample.INSTANCE;
+		EagerShoppingCartExample example2 = EagerShoppingCartExample.INSTANCE;
+
+		if (example == example2) {
+			System.out.println("obiectele sunt identice");
+		}
+		
+		SingletonEnum mySingleton = SingletonEnum.INSTANCE;
+		mySingleton.getProducts();
+
+	}
 }
