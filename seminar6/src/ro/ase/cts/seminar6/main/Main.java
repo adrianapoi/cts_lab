a07aace63229113cfbef9ad15c12f8ed1c10e778
AdrianaPoi <poinarescuadriana18@stud.ase>
2021-04-01 09:24:09 +0300

    seminar6 final

--- seminar6/src/ro/ase/cts/seminar6/main/Main.java
+++ seminar6/src/ro/ase/cts/seminar6/main/Main.java
@@ -7,35 +7,37 @@ import ro.ase.cts.seminar6.builder.Product;
 import ro.ase.cts.seminar6.singleton.Cart;
 import ro.ase.cts.seminar6.builder.TechProductFactory;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		Cart myShoppingCart = Cart.getInstance("shopping");
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Selectati categoria de produse:\n Produse tech \n Produse office");
 		String userPreferences = scan.nextLine();
 		Product myProduct = null;
-		
+
 		AbstractProductFactory productFactory = null;
-		if(userPreferences != null) {
-			if(userPreferences.equalsIgnoreCase("tech")) {
+		if (userPreferences != null) {
+			if (userPreferences.equalsIgnoreCase("tech")) {
 				productFactory = new TechProductFactory();
 			}
 		}
-		myProduct = productFactory.makeProduct();
-		
-//		}else {
-//			System.out.println("Optiune invalida");
-//			System.out.println("Catalog de produse:\n Produse tech \n  Produse office");
-//		}
-		
-		if(myProduct !=null) {
+
+		System.out.println(productFactory.getCatalog());
+		userPreferences = scan.nextLine();
+		try {
+			int selectedId = Integer.valueOf(userPreferences);
+			myProduct = productFactory.makeProduct(selectedId);
+		} catch (NumberFormatException e) {
+			System.err.println("Selectie invalida");
+		}
+
+		if (myProduct != null) {
 			myShoppingCart.products.add(myProduct);
 		}
-		
-		for (Product p: myShoppingCart.products) {
+
+		for (Product p : myShoppingCart.products) {
 			System.out.println(p.getDescription());
 		}
 	}
