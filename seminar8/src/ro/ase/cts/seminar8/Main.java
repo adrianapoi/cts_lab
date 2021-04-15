adbf69d78a4b1736fdda3df543df0287bd1b15e7
AdrianaPoi <poinarescuadriana18@stud.ase>
2021-04-15 08:21:26 +0300

    seminar8 breaj

---
+++ seminar8/src/ro/ase/cts/seminar8/Main.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.seminar8;
+
+import ro.ase.cts.seminar8.facade.OrderFacade;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		OrderFacade facade = new OrderFacade();
+		facade.order();
+
+	}
+
+}
