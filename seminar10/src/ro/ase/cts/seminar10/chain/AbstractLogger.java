948df0cb41f99cf613b4e0cd453371a4b8149edd
AdrianaPoi <poinarescuadriana18@stud.ase>
2021-05-06 08:30:01 +0300

    seminar10 break

---
+++ seminar10/src/ro/ase/cts/seminar10/chain/AbstractLogger.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.seminar10.chain;
+
+public abstract class AbstractLogger {
+	protected Verbosity level;
+	private AbstractLogger nextLogger;
+	public void setNextLogger(AbstractLogger logger) {
+		this.nextLogger=logger;
+	}
+	
+	public void logMessage(Verbosity level, String message)
+	{
+		if(this.level.getVerbosity()<=level.getVerbosity())
+		{
+			
+		}
+	}
+	
+	abstract void write(String message);
+	
+}
