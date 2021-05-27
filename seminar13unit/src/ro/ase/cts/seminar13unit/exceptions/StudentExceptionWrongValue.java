d6225a27d7d0f58781ce8cb18dc7947b061b9a96
AdrianaPoi <poinarescuadriana18@stud.ase>
2021-05-27 08:28:46 +0300

    seminar13 break

---
+++ seminar13unit/src/ro/ase/cts/seminar13unit/exceptions/StudentExceptionWrongValue.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.seminar13unit.exceptions;
+
+public class StudentExceptionWrongValue extends Exception {
+public StudentExceptionWrongValue(String message) {
+	super(message);
+}
+}
